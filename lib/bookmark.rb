dc700dbfb3e1baa23913a260823f1d65dce9806a
Ben Partington <bdpartington@gmail.com>
2022-02-17 15:46:38 +0000

    Step 11, modifications to return Bookmark object rather than string

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,6 +2,14 @@ require 'pg'
 
 class Bookmark
 
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
+
   def self.list_all
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
@@ -10,7 +18,7 @@ class Bookmark
     end
 
     result = connection.exec('SELECT * FROM bookmarks')
-    result.map { |bookmark| bookmark['url'] }
+    result.map { |bookmark| Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) }
   end
 
   def self.create(url:, title:)
@@ -20,6 +28,6 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
     
-    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}, '#{url}') RETURNING id, url, title")
+    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
   end
 end
